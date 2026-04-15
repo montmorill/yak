d0e209b131b6301423e305c80315bbb809fd9813
montmorill <montmorill@outlook.com>
2026-04-15 19:44:59 +0800

    Updates ESLint and TypeScript configurations
    Disables the multiline-ternary rule for more flexible code formatting
    Updates the JSX fragment factory to use h.template for compatibility
    Removes the noPropertyAccessFromIndexSignature restriction to simplify type safety rules
    Updates the @types/node dependency to the latest version

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   rules: {
+    'multiline-ternary': 'off',
     'style/operator-linebreak': ['error', 'before', {
       overrides: {
         '=': 'after',
